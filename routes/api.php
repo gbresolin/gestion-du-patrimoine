578658ff69764e4a2e318dfaf54803e4771238f0
gbresolin <gregory.bresolin@ipilyon.net>
2020-06-25 09:32:47 +0200

    Protection API Monument avec api_token

--- routes/api.php
+++ routes/api.php
@@ -17,14 +17,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('monuments', 'ApiController@getAllMonuments');
-Route::get('monument/{id}', 'ApiController@getMonument');
+Route::middleware('auth:api')->get('monuments', 'ApiController@getAllMonuments');
+Route::middleware('auth:api')->get('monument/{id}', 'ApiController@getMonument');
 Route::post('monument/create', 'ApiController@createMonument');
 Route::put('monument/update/{id}', 'ApiController@updateMonument');
 Route::delete('monument/delete/{id}','ApiController@deleteMonument');
 
 Route::get('users', 'ApiController@getAllUsers');
-Route::middleware('auth:api')->get('user/{id}', 'ApiController@getUser');
+Route::get('user/{id}', 'ApiController@getUser');
 Route::post('user/create', 'ApiController@createUser');
 Route::put('user/update/{id}', 'ApiController@updateUser');
 Route::delete('user/delete/{id}','ApiController@deleteUser');
