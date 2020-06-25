6442c6f453386d2bc6cec257a1a61154d9eb8290
gbresolin <gregory.bresolin@ipilyon.net>
2020-06-25 10:58:25 +0200

    MAJ page accueil

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,14 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
+        <!-- CSS only -->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+        <!-- JS, Popper.js, and jQuery -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
         <!-- Styles -->
         <style>
             html, body {
@@ -81,18 +89,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Gestion du Patrimoine
                 </div>
+                <h2>BDD et API</h2>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="mt-3">
+                    <span class="font-weight-bold">KHALDI</span> -
+                    <span class="font-weight-bold">ESTIVAL</span> -
+                    <span class="font-weight-bold">BRESOLIN</span> -
+                    <span class="font-weight-bold">MAGNIN</span>
                 </div>
             </div>
         </div>
